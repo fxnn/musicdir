4024ec970bb9eca6649025e6f5132fcecc7ce912
Felix Neumann <dev@fxnn.de>
2020-04-18 12:53:51 +0200

    main.py: use argparse

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 #!/bin/env python
 
-import sys
+import argparse
 import os
+import sys
 
 import scan
 
 def main():
-    basedir=sys.argv[1]
-    if not basedir:
-        print "usage: %s basedir" % sys.argv[0]
-        return 1
-    basedir=os.path.abspath(basedir)
+    parser = argparse.ArgumentParser(description='Scan and alter music directories.')
+    parser.add_argument('basedir', help='The directory to work on')
+    args = parser.parse_args()
+
+    basedir=os.path.abspath(args.basedir)
     musicdirs=scan.scan_dir(basedir)
-    for musicdir in musicdirs:
-        if not musicdir.contains_music() and not musicdir.contains_dirs():
-            if musicdir.contains_files():
-                print('no music: %s' % musicdir.abspath)
+
+    for m in musicdirs:
+        if not m.contains_music() and not m.contains_dirs():
+            if m.contains_files():
+                print('no music: %s' % m.abspath)
             else:
-                print('empty: %s' % musicdir.abspath)
+                print('empty: %s' % m.abspath)
+
     return 0
 
 if __name__ == '__main__':
